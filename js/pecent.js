fc3a24ab440bbefbe1f2ff976e41cd2060d0ff75
ELEILSON SANTOS <105988025+Eleilson69@users.noreply.github.com>
2022-08-08 02:44:34 -0300

    update

--- js/pecent.js
+++ js/pecent.js
@@ -11,32 +11,77 @@ const numberPercent3 = document.querySelector('.number-percent3')
 const numberPercent4 = document.querySelector('.number-percent4')
 const numberPercent5 = document.querySelector('.number-percent5')
 
-const values = [85, 75, 70, 65, 60]
+const values = [90, 86, 73, 69, 76]
 
 const percent1 = () => {
     const number = values[0]
-    numberPercent1.innerHTML = `${number} <span>%</span>`
     circleProgres.style.strokeDashoffset = 189 - (189 * number) / 100
+
+    let counter = 0
+
+    const timer = setInterval(() => {
+        counter++
+        if (counter === number) {
+            clearInterval(timer)
+        }
+        numberPercent1.innerHTML = `${counter} <span>%</span>`
+    }, 25)
 }
 const percent2 = () => {
     const number = values[1]
-    numberPercent2.innerHTML = `${number} <span>%</span>`
     circleProgres2.style.strokeDashoffset = 189 - (189 * number) / 100
+
+    let counter = 0
+
+    const timer = setInterval(() => {
+        counter++
+        if (counter === number) {
+            clearInterval(timer)
+        }
+        numberPercent2.innerHTML = `${counter} <span>%</span>`
+    }, 25);
 }
 const percent3 = () => {
     const number = values[2]
-    numberPercent3.innerHTML = `${number} <span>%</span>`
     circleProgres3.style.strokeDashoffset = 189 - (189 * number) / 100
+
+    let counter = 0
+
+    const timer = setInterval(() => {
+        counter++
+        if (counter === number) {
+            clearInterval(timer)
+        }
+        numberPercent3.innerHTML = `${counter} <span>%</span>`
+    }, 25);
 }
 const percent4 = () => {
     const number = values[3]
-    numberPercent4.innerHTML = `${number} <span>%</span>`
     circleProgres4.style.strokeDashoffset = 189 - (189 * number) / 100
+
+    let counter = 0
+
+    const timer = setInterval(() => {
+        counter++
+        if (counter === number) {
+            clearInterval(timer)
+        }
+        numberPercent4.innerHTML = `${counter} <span>%</span>`
+    }, 25);
 }
 const percent5 = () => {
     const number = values[4]
-    numberPercent5.innerHTML = `${number} <span>%</span>`
     circleProgres5.style.strokeDashoffset = 189 - (189 * number) / 100
+
+    let counter = 0
+
+    const timer = setInterval(() => {
+        counter++
+        if (counter === number) {
+            clearInterval(timer)
+        }
+        numberPercent5.innerHTML = `${counter} <span>%</span>`
+    }, 25);
 }
 
 const load = () => {
